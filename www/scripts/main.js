e4b2227d26d336df4ec7d0321049f8f7cd11976c
Sam Ebertz <samuel.ebertz@gmail.com>
2018-01-20 18:45:55 -0600

    updated p5
    [+] updated p5 to 2017-01-19 release
    [+] added version from u/spongman with some webgl fixes that haven't
    been merged as 'p5_x'

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -2,7 +2,7 @@ requirejs.config({
   baseUrl: 'scripts',
   paths: {
     lib: 'lib',
-    p5: 'lib/p5/p5.min',
+    p5: 'lib/p5/p5_x.min',
     v3d: 'lib/V3D',
     sketch_utils: 'lib/sketch_utils'
   }
@@ -10,6 +10,7 @@ requirejs.config({
 
 requirejs(['p5', 'v3d', 'geometry', 'sketch_utils'],
 function   (p5,   v3d,   geometry,   sketch_utils) {
+  var fillScale = 0.999, strokeScale = 1.0/fillScale
   var sketch = new p5(function(sketch) {
     sketch_utils.bind_to_sketch(sketch)
 
@@ -19,9 +20,15 @@ function   (p5,   v3d,   geometry,   sketch_utils) {
       sketch.camera(4, 12, 6.2, 0, 0, .2, 0, 0, -1)
       sketch.perspective(sketch.PI/12, sketch.width/sketch.height, 1, 50)
       sketch.noStroke()
-      sketch.noFill()
+      // sketch.stroke(255)
+      sketch.strokeWeight(0.01)
+      // sketch.fill('rgba(255,255,255,0.5)')
+      sketch.fill(0)
       // icosahedron = sketch.loadModel('../assets/blender_icosahedron.obj',true)
       icosahedron = geometry
+      icosahedron.subdivide_and_project()
+      icosahedron.subdivide_and_project()
+      // icosahedron.subdivide_and_project()
       icosahedron.draw = icosahedron.draw.bind(sketch)
       sketch.setFrameRate(30)
       // sketch.noLoop()
@@ -30,11 +37,17 @@ function   (p5,   v3d,   geometry,   sketch_utils) {
     sketch.draw = function() {
       sketch.background(72)
       // sketch.orbitControl()
-      // sketch.utils.drawAxes(1.5)
-      sketch.strokeWeight(0.05)
-      // sketch.scale(.01, .01, .01)
-      // sketch.model(icosahedron)
+      sketch.rotateZ(sketch.millis()/2000)
+      sketch.utils.drawAxes(1.5)
+      // sketch.noStroke()
+      sketch.stroke(255)
+      sketch.noFill()
       icosahedron.draw()
+
+      // sketch.noStroke()
+      sketch.stroke(200,100,200)
+      sketch.fill(0)
+      sketch.box(.75)
     }
   }, 'p5_wrapper')
 });
